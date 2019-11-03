657b1abdaf72403a3ad9905ca5e073e659a0bdd2
Jan Everaert <jan.everaert@imec.be>
2019-11-03 12:47:51 +0100

    fix pool error

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,9 @@ class App {
   constructor(opts) {
     this.pg = require("knex")({
       client: "pg",
-      connection: process.env.PG_CONNECTION_STRING
+      version: '9.6',
+      connection: process.env.PG_CONNECTION_STRING,
+      searchPath: ['knex', 'public']
     });
 
     const _this = this;
